d8adb9880b58ac02f92f211ae9146774148ecbf3
Denise Pendleton <dpendl1@hotmail.com>
2018-03-16 02:11:24 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -30,8 +30,9 @@ end
 
 def list_artists
   arr = Artist.all.sort_by(&:name)
+  binding.pry
   arr.each.with_index(1) do |art, idx|
-    binding.pry
+    
     puts "#{idx}. #{art.name}"
       end
 end
