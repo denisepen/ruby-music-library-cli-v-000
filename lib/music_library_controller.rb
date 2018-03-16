e5b37500762b00cc4845dd108ce115a61f07c90d
Denise Pendleton <dpendl1@hotmail.com>
2018-03-16 02:43:37 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -67,7 +67,7 @@ def list_songs_by_artist
     end
   end
   #
-  def play_song(input)
+  def play_song
     puts "Which song number would you like to play?"
     input = gets.strip.to_i
     if (1..Song.all.length).include?(input)
