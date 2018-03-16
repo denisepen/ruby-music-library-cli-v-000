22a6b29e0eab39614c0504ac10678157cbc43352
Denise Pendleton <dpendl1@hotmail.com>
2018-03-16 02:35:24 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -56,23 +56,23 @@ def list_songs_by_artist(input)
     end
   end
 
-  def list_songs_by_genre(input)
-    puts "Please enter the name of a genre:"
-    input = gets.strip
-
-    if genre = Genre.find_by_name(input)
-      genre.songs.sort_by(&:name).each.with_index(1) do |song, idx|
-        puts "#{idx}. #{song.artist.name} - #{song.name}"
-      end
-    end
-  end
-
-  def play_song(input)
-    puts "Which song number would you like to play?"
-    input = gets.strip.to_i
-    if (1..Song.all.length).include?(input)
-    song = Song.all.sort_by(&:name)[input -1]
-  end
-    puts "Playing #{song.name} by #{song.artist.name}" if song
-  end
+  # def list_songs_by_genre(input)
+  #   puts "Please enter the name of a genre:"
+  #   input = gets.strip
+  #
+  #   if genre = Genre.find_by_name(input)
+  #     genre.songs.sort_by(&:name).each.with_index(1) do |song, idx|
+  #       puts "#{idx}. #{song.artist.name} - #{song.name}"
+  #     end
+  #   end
+  # end
+  #
+  # def play_song(input)
+  #   puts "Which song number would you like to play?"
+  #   input = gets.strip.to_i
+  #   if (1..Song.all.length).include?(input)
+  #   song = Song.all.sort_by(&:name)[input -1]
+  # end
+  #   puts "Playing #{song.name} by #{song.artist.name}" if song
+  # end
 end
